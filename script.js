2d3e54ade4422e3cf20c673094585a4b2271d805
Lucas Andrade <lucasandrade713@gmail.com>
2021-06-26 17:47:24 -0300

    add more code

--- script.js
+++ script.js
@@ -14,16 +14,16 @@ for (const link of links) {
   })
 }
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remover('scroll')
   }
-})
+}
 
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
@@ -46,7 +46,23 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
